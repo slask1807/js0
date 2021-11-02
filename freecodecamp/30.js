6c1aa6ac13d97bdeddfbd20ef28cf5cdf26d7b03
“Dzmitry <slask@tut.by>
2021-11-02 23:08:46 +0300

    8

---
+++ freecodecamp/30.js
@@ -0,0 +1,11 @@
+function giveMeFive(obj){
+    const five=[];
+    for (var key in obj){
+        if (key.length==5){
+            five.push(key);}
+        if (obj[key].length==5){
+            five.push(obj[key]);}
+
+    }
+    return five;
+}
